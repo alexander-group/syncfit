2158d437550468b0c36615cd528f60987b69e432
Noah Franz <nfranz@arizona.edu>
2024-03-20 14:33:16 -0700

    update init

--- src/syncfit/__init__.py
+++ src/syncfit/__init__.py
@@ -1,4 +1,8 @@
 from ._version import __version__
 
 import syncfit.mcmc
-import syncfit.single_break_model
+import syncfit.b5_model
+import syncfit.b4b5_model
+import syncfit.b4b5b3_model
+import syncfit.b1b2_model
+import b1b2_b3b4_weighted_model
