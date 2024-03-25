7a6bf315c2288da1f4ece86cff35eef83b7f4b87
Noah Franz <noahfranz13@gmail.com>
2024-03-25 15:24:41 -0700

    update how we plot

--- src/syncfit/mcmc.py
+++ src/syncfit/mcmc.py
@@ -15,6 +15,9 @@ def do_emcee(theta_init, nu, F_muJy, F_error, model_name='b5', niter=2000, day=N
     day: day of observation (string)
     theta_init: array of initial guess 
     model_name: Shortened model name to use. Options are b5, b4b5, b4b5b3, b1b2, b1b2_b3b4_weighted
+
+    Returns:
+    flat_samples, log_prob
     """
         
     # import the relevant module
@@ -33,8 +36,10 @@ def do_emcee(theta_init, nu, F_muJy, F_error, model_name='b5', niter=2000, day=N
     pos, prob, state = sampler.run_mcmc(pos, niter, progress=True);
 
     flat_samples = sampler.get_chain(discard=niter//2, flat=True)
-
+    log_prob = sampler.get_log_prob(discard=niter//2, flat=True)
+    
     if plot:
+        
         #Plot chains
         fig, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
         samples = sampler.get_chain()
@@ -56,15 +61,18 @@ def do_emcee(theta_init, nu, F_muJy, F_error, model_name='b5', niter=2000, day=N
             txt = "\mathrm{{{3}}} = {0:.2e}_{{-{1:.3f}}}^{{{2:.3f}}}"
             txt = txt.format(mcmc[1], q[0], q[1], labels[i])
             print(txt)
-
+            
+        # get the best 100 of the chain (ie where log_prob is maximum)
+        # then plot these
+        nkeep = 1000
+        #import pdb; pdb.set_trace()
+        toplot = flat_samples[np.argsort(log_prob)[-nkeep:]]
+        
         nu_plot = np.arange(1e8,3e11,1e7)
         fig = plt.figure(figsize = (4,4))
         ax = plt.subplot(111)
-        v = len(flat_samples.transpose()) - 1000 # only plot the last thousange
-        for i in range(len(flat_samples.transpose()[0][v:])):
-            ax.plot(nu_plot, model.SED(nu_plot,
-                                       *[val[v:][i] for val in flat_samples.transpose()]
-                                       ),
+        for val in toplot:
+            ax.plot(nu_plot, model.SED(nu_plot, *val),
                     '-', color='k', lw = 0.5, alpha = 0.1)
 
         ax.errorbar(emcee_args['nu'], emcee_args['F'], fmt='o', markeredgecolor='black',
@@ -77,4 +85,4 @@ def do_emcee(theta_init, nu, F_muJy, F_error, model_name='b5', niter=2000, day=N
         ax.set_xscale('log')
         plt.show()
     
-    return flat_samples
+    return flat_samples, log_prob
