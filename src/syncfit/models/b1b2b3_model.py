4a3d6ffe7622ea702c71e54b4e06010a690064f3
Collin Christy <82454849+collinchristy@users.noreply.github.com>
2026-01-15 16:37:43 -0700

    Update b1b2b3_model.py

--- src/syncfit/models/b1b2b3_model.py
+++ src/syncfit/models/b1b2b3_model.py
@@ -49,7 +49,7 @@ class B1B2B3(SyncfitModel):
         nu_a = 10**log_nu_a
         nu_c = 10**log_nu_c
 
-        term1 = ( (nu/nu_m)**(b1) * np.exp(-s1*(nu/nu_m)**(2/3)) + (nu/nu_m)**(b2))
+        term1 = ( (nu/nu_a)**(-s1*b1) + (nu/nu_a)**(-s1*b2) )**(-1/s1)
         term2 = ( 1 + (nu/nu_a)**(s2*(b2-b3)) )**(-1/s2)
         term3 = ( 1 + (nu/nu_c)**(s3*(b3-b4)) )**(-1/s3)
 
